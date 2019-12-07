3ea6356501b6511239528cb96080ae9156e8ceb9
slametrianto <slametrianto25@gmail.com>
2019-12-07 22:12:26 +0700

    energitermal

---
+++ application/views/blog/artikelsatu.php
@@ -0,0 +1,101 @@
+<div class="jumbotron" style="background-image: url('assets/img/')">
+    <div class="container">
+        <!-- <h1 class=" display-4 text-white">Hello, world!</h1> -->
+        <!-- <div class="post-heading">
+            <h1 class="text-white">Man must explore, and this is exploration at its greatest</h1>
+            <h2 class="subheading text-white">Problems look mighty small from 150 miles up</h2>
+            <span class="meta text-white">Posted by
+                <a href="#">CV. Energi Termal Solusi</a>
+                on August 24, 2019</span>
+        </div> -->
+
+
+    </div>
+</div>
+
+
+<!-- Post Content -->
+<article>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <h2 class="section-heading">Vertiv SmartCabinet : Ruang Server Mini Dalam Satu Kabinet</h2>
+                <p> Ingin membangun ruang server tapi bingung karena workspace yang tersedia tidak cukup luas? Atau, khawatir dengan waktu yang dibutuhkan untuk proses develop hingga pembangunan ruang server tersebut, serta kebisingan yang akan dihasilkan?</p>
+
+                <p>Jangan lupakan masalah cooling management yang dibutuhkan agar suhu di dalam ruang server tetap terjaga dengan baik, serta kebutuhan UPS yang wajib ada untuk menopang distribusi back up daya yang dibutuhkan oleh unit server Anda.</p>
+
+                <p>Memperkenalkan, Vertiv SmartCabinet (MSC-A5S652BMNNXX), intelligent and integrated containment for IT Infrastructure.</p>
+
+                <p>Dengan solusi “ruang server mini dalam 1 kabinet” dari Vertiv (sebelumnya dikenal dengan nama Emerson Liebert), segala kerepotan di atas akan langsung sirna. Karena solusi SmartCabinet (MSC-A5S652BMNNXX) dari Vertiv dapat menjawab segala kebutuhan yang Anda butuhkan untuk membangun ruang server milik Anda.</p>
+
+                <p>Dengan SmartCabinet (MSC-A5S652BMNNXX) yang bahkan gampang untuk dirakit dan dipindahkan, Anda dapat memiliki ruang server mini dalam 1 kabinet yang telah dilengkapi dengan sistem cooling (thermal management & emergency fans), unit UPS & batteries, sentralisasi sistem monitoring, panel LCD display yang intuitif, built-in Power Management Unit, serta lighting system untuk menerangi bagian interior dalam kabinet.</p>
+
+                <h2 class="section-heading">FITUR STANDAR SMARTCABINET</h2>
+
+                <p>Perlindungan dari debu dan pengurangan kebisingan, efisiensi tinggi dan penghematan energi: sistem operasi tertutup dan siklus internalnya memastikan temperatur dan kelembaban di dalam kabinet, untuk memperpanjang masa pakai perangkat IT. UPS yang sangat efisien dan pendingin AC Presisi dengan teknologi cold/hot aisle untuk tujuan meningkatkan efek pendinginan. Fan/Kipas ini didesain dengan kebisingian silent/diam, yang bisa digunakan di area perkantoran.</p>
+
+                <p> Intelligent monitoring: fungsi intelligent control seperti pemantauan lingkungan terpadu, pemantauan perangkat, hubungan alarm dan sebagainya menyediakan platform pemantauan terpusat untuk pengelolaan ruang komputer.</p>
+
+                <!-- <blockquote class="blockquote">merupakan jenis UPS yang dipakai untuk para pemilik bisnis kecil yang sudah membutuhkan jumlah arus listrik yang lebih besar. Kemampuannya di atas UPS standby dengan harga yang juga tentunya lebih mahal. Beberapa server milik pemerintah juga banyak yang menggunakan UPS jenis ini. Tingkat reliabilitasnya lebih tinggi dan mampu menyesuaikan jumlah voltase yang lebih tinggi dari peralatan.</blockquote> -->
+
+                <p> Sangat terintegrasi dan menghemat ruang: satu kabinet standar menyediakan kondisi operasi yang stabil untuk semua perangkat IT, hanya koneksi kabinet dan pemasangan AC yang dibutuhkan di sisi user. Sistem kabinet ini hanya menempati 0,7 meter persegi.</p>
+
+                <h2 class="section-heading">UPS Double Conversion Online</h2>
+
+                <p>Tipe ini merupakan yang paling lazim untuk UPS dengan daya lebih dari 10kVA. Tipe ini memiliki kesamaan dengan tipe Standby. Hanya saja tipe ini memiliki sumber tenaga utama yang terletak pada Inverter, bukan pada sumber listrik AC. Dapat dikatakan tipe ini mendekati gambaran ideal dari sebuah UPS, sayangnya tipe ini menghasilkan panas yang cukup tinggi.</p>
+
+                <a href="#">
+                    <img class="img-fluid" src="assets/img/ime.jpg" alt="">
+                </a>
+                <span class="caption text-muted">To go places and do things that have never been done before – that’s what living is all about.</span>
+
+                <p>Friendly Human Machine Interface (HMI): tampilan layar dengan lebar 7 inci yang besar membuat Anda mengetahui informasi pengoperasian / alarm / keselamatan perangkat. Melalui kartu monitor yang terintegrasi di kabinet, Anda bisa menerapkan pemantauan jarak jauh di ruang komputer mini sekalipun, untuk memenuhi permintaan yang berbeda.</p>
+
+                <p>Pengiriman cepat dan brand yang terpecaya: disiapkan untuk “one go”, dibangun dalam satu jam, dan dapat dinyalakan dalam satu hari.</p>
+
+                <!-- <p>Placeholder text by
+                    <a href="http://spaceipsum.com/">Space Ipsum</a>. Photographs by
+                    <a href="https://www.flickr.com/photos/nasacommons/">NASA on The Commons</a>.</p> -->
+            </div>
+        </div>
+    </div>
+</article>
+
+<hr>
+
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <ul class="list-inline text-center">
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#">
+                            <span class="fa-stack fa-lg">
+                                <i class="fas fa-circle fa-stack-2x"></i>
+                                <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+                <p class="copyright text-muted">Blog &copy; Energi Termal Solusi Your Website 2019</p>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ application/views/templates/pex.php
@@ -0,0 +1,42 @@
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="assets/img/sept.jpg" class="d-block w-100" height="500">
+            <div class="carousel-caption d-none d-md-block">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="alert alert-secondary" role="alert">
+    product & solution > termal management (AC) > pex
+</div>
+
+
+<section>
+    <div class="container mt-3" id="pex">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card border-primary mb-3">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="assets/img/download.jpg" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title text-primary">Smart Cabinet</h5>
+                                <p class="card-text text-justify">The Liebert PEX+ is designed as a modular-type room-based precision cooling system for IT critical equipments that require a high level of sensible heat elimination and precise temperature and humidity control. It is available in air-cooled (20 - 100kW) configurations to cover a wide range of room applications.Notes: The New Liebert PEX+ will focus to sell models with Cooling, Electric Re-heater and Humidifier components.For Special Applications that requires Indoor units WITHOUT Electric Re-heater and Humidifier components consult Emerson Representative for approval and SFA pricing.Consult Emerson Representative for requirements with R407C refrigerant.
+                                </p>
+                                <p class="card-text"><small class="text-muted">Smart Cabinet : EDGE of Nework, Office Branches, Retail, Education.</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+    </div>
+</section>
